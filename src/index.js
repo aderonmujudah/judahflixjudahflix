2cbe8e90c8016f5d90ee7619f92912b20a8623c4
Judah Aderonmu <judahadetunji@gmail.com>
2023-04-06 15:52:08 +0100

    setting up jumbotron
    setting up the home page of the site

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
+import { render } from "react-dom";
+import "normalize.css";
 import App from "./app";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+render(<App />, document.getElementById("root"));
