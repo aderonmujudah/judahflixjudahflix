ac70ba3885b502ac33e177013618333d6c582f5b
Judah Aderonmu <judahadetunji@gmail.com>
2023-04-06 16:02:34 +0100

    update jumbtron.js

--- src/components/jumbotron/styles/jumbotron.js
+++ src/components/jumbotron/styles/jumbotron.js
@@ -16,12 +16,29 @@ export const Inner = styled.div`
 export const Pane = styled.div`
   width: 50%;
 `;
-export const Title = styled.h1``;
+export const Title = styled.h1`
+  font-size: 50px;
+  line-height: 1.1;
+  margin-bottom: 8px;
 
-export const SubTitle = styled.h2``;
+  @media (max-width: 600px) {
+    font-size: 35px;
+  }
+`;
 
-export const Image = styled.image``;
+export const SubTitle = styled.h2`
+  font-size: 26px;
+  font-weight: normal;
+  line-height: normal;
 
-export const Item = styled.div``;
+  @media (max-width: 600px) {
+    font-size: 18px;
+  }
+`;
+
+export const Image = styled.img`
+  max-width: 100%;
+  height: auto;
+`;
 
 export const Container = styled.div``;
