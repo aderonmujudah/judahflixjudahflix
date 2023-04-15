39171ad55b69fee2223ebea53ac70c058fa25608
Judah Aderonmu <judahadetunji@gmail.com>
2023-04-15 22:44:30 +0100

    completed homepage

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,4 +1,6 @@
 import React from "react";
+import { Feature } from "../components";
+import { OptForm } from "../components";
 import { HeaderContainer } from "../containers/header";
 import { JumbotronContainer } from "../containers/jumbotron";
 import { FaqsContainer } from "../containers/faq";
@@ -7,7 +9,26 @@ import { FooterContainer } from "../containers/footer";
 export default function Home() {
   return (
     <>
-      <HeaderContainer></HeaderContainer>
+      <HeaderContainer>
+        <Feature>
+          <Feature.Title>
+            Unlimited films, TV programmes and more.
+          </Feature.Title>
+          <Feature.SubTitle>
+            Watch anywhere. Cancel at any time.
+          </Feature.SubTitle>
+
+          <OptForm>
+            <OptForm.Input placeholder="Email Adress" />
+            <OptForm.Button>Get Started</OptForm.Button>
+            <OptForm.Break />
+            <OptForm.Text>
+              Ready to watch? Enter your email to create or restart your
+              membership
+            </OptForm.Text>
+          </OptForm>
+        </Feature>
+      </HeaderContainer>
       <JumbotronContainer />
       <FaqsContainer />
       <FooterContainer />
